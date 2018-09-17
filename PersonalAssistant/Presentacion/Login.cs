b08f81a2151388efbf79e87b165a7ac2386f725b
Fernando <fermg299@outlook.com>
2018-09-16 21:17:37 -0300

    Diseño de Login

---
+++ PersonalAssistant/Presentacion/Login.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "USUARIO")
+            {
+                txtUsuario.Text = "";
+                txtUsuario.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "USUARIO";
+                txtUsuario.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtClave_Enter(object sender, EventArgs e)
+        {
+            if (txtClave.Text == "CLAVE")
+            {
+                txtClave.Text = "";
+                txtClave.ForeColor = Color.DimGray;
+                txtClave.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtClave_Leave(object sender, EventArgs e)
+        {
+            if (txtClave.Text == "")
+            {
+                txtClave.Text = "CLAVE";
+                txtClave.ForeColor = Color.Silver;
+                txtClave.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCerrar_MouseEnter(object sender, EventArgs e)
+        {
+            btnCerrar.Visible = false;
+            btnCerrar2.Visible = true;
+        }
+
+        private void btnCerrar2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCerrar2_MouseLeave(object sender, EventArgs e)
+        {
+            btnCerrar2.Visible = false;
+            btnCerrar.Visible = true;
+        }
+    }
+}
